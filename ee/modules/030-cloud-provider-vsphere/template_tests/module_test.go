51683b9450f1ea37411b21131215e035b55b2b7e
Yuriy Losev <yuriy.losev@flant.com>
2023-02-06 20:21:06 +0400

    ++
    Signed-off-by: Yuriy Losev <yuriy.losev@flant.com>

--- ee/modules/030-cloud-provider-vsphere/template_tests/module_test.go
+++ ee/modules/030-cloud-provider-vsphere/template_tests/module_test.go
@@ -40,7 +40,7 @@ const globalValues = `
     clusterType: Cloud
     defaultCRI: Docker
     kind: ClusterConfiguration
-    kubernetesVersion: "1.25"
+    kubernetesVersion: "1.21"
     podSubnetCIDR: 10.111.0.0/16
     podSubnetNodeCIDRPrefix: "24"
     serviceSubnetCIDR: 10.222.0.0/16
@@ -51,7 +51,7 @@ const globalValues = `
       worker: 1
       master: 3
     podSubnet: 10.0.1.0/16
-    kubernetesVersion: 1.25.11
+    kubernetesVersion: 1.21.11
 `
 
 const moduleValuesA = `
